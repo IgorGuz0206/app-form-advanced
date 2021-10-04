51de7a759df2d0287b6b333bfe73026e73aba01e
IgorGuz0206 <guzovatyi1993dp@gmail.com>
2021-10-04 18:42:08 +0300

    Валидация в компоненте

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../user-class';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
+  roles: string[] = ['Гость', 'Модератор', 'Администратор'];
+  model: User = new User(1, '', '', 0);
 
-  roles: string[] = ['Гость', 'Модератор', 'Администратор']
-  model: User = new User(1, '', '', 0)
+  formErrors: any = {
+    name: '',
+    age: '',
+  };
 
-  constructor() { }
+  validationMessages: any = {
+    name: {
+      required: 'Имя обязательно',
+      minlength: 'Имя должно содержать минимум 4 символа',
+    },
+    age: {
+      required: 'Возраст обязателен',
+    },
+  };
 
-  ngOnInit(): void {
+  @ViewChild('userForm') userForm: NgForm | null = null;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.userForm?.valueChanges?.subscribe(() => this.onValueChanged());
   }
 
+  onSubmit(): void {
+    console.log('Submitted');
+  }
+
+  private onValueChanged(data?: any): void {
+    const form: any = this.userForm?.form;
+
+    Object.keys(this.formErrors).forEach((field) => {
+      this.formErrors[field] = '';
+
+      const control = form.get(field);
+
+      if (control && control.dirty && control.invalid) {
+        const message = this.validationMessages[field];
+
+        Object.keys(control.errors).forEach((key) => {
+          this.formErrors[field] += message[key] + ' ';
+        });
+      }
+    });
+  }
 }
