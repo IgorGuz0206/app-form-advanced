38488c0e5154a678986f430a9af1f1abdb1805e3
IgorGuz0206 <guzovatyi1993dp@gmail.com>
2021-10-11 15:37:18 +0300

    FormBuilder

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../user-class';
-import { FormControl, FormGroup, Validators, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators, NgForm, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,12 +9,12 @@ import { FormControl, FormGroup, Validators, NgForm } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  userForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required, Validators.minLength(7)])
+  userForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    password:['', [Validators.required, Validators.minLength(7)]]
   })
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     
   }
 
