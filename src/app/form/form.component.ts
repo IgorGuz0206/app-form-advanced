1fc6acfe53907281e9b7eaaaecabc7c93733ffaf
IgorGuz0206 <guzovatyi1993dp@gmail.com>
2021-10-12 16:41:39 +0300

    Обработка полей Bootstrap

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,7 @@ import {
   NgForm,
   FormBuilder,
 } from '@angular/forms';
+import { emailValidator, rangeValidator } from '../custom-validators';
 
 @Component({
   selector: 'app-form',
@@ -19,73 +20,108 @@ export class FormComponent implements OnInit {
 
   userForm!: FormGroup;
 
+  formSuccess = {
+    name: 'Принято!',
+    password: 'Принято!',
+    email: 'Принято!',
+    age: 'Принято!',
+    role: 'Принято!',
+  };
+
+  formPlaceholders = {
+    name: 'Введите логин...',
+    password: 'Введите пароль...',
+    email: 'Укажите адрес электронной почты...',
+    age: 'Установите возраст...',
+    role: 'Выберите роль из списка...',
+  };
+
+  formLabels = {
+    name: 'Логин',
+    password: 'Пароль',
+    email: 'Email',
+    age: 'Установите возраст...',
+    role: 'Роль',
+  };
+
   formErrors: any = {
     name: '',
     password: '',
     email: '',
     age: '',
-    role: ''
+    role: '',
   };
 
   validationMessages: any = {
     name: {
       required: 'Имя обязательно',
       minlength: 'Имя должно содержать не менее 4 символов',
-      maxlength: 'Имя должно содержать не более 15 символов'
+      maxlength: 'Имя должно содержать не более 15 символов',
     },
     password: {
       required: 'Пароль обязателен',
       minlength: 'Имя должно содержать не менее 7 символов',
-      maxlength: 'Имя должно содержать не более 25 символов'
+      maxlength: 'Имя должно содержать не более 25 символов',
     },
     email: {
       required: 'Email обязателен',
-      email: 'Неправильний формат email адреса'
+      emailValidator: 'Неправильний формат email адреса',
     },
     age: {
       required: 'Возраст обязателен',
-      pattern: 'Значение должно быть числом'
+      rangeValidator: 'Значение должно быть числом в диапазоне 1...122',
     },
     role: {
-      required: 'Обязательное поле'
-    }
+      required: 'Обязательное поле',
+    },
   };
 
-  constructor(private formBuilder: FormBuilder) {
-   
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.buildForm()
+    this.buildForm();
   }
 
   onSubmit(): void {}
 
   private buildForm(): void {
     this.userForm = this.formBuilder.group({
-      name: [this.user.name, [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
-      password: [this.user.password, [Validators.required, Validators.minLength(7), Validators.maxLength(25)]],
-      email: [this.user.email, [Validators.required, Validators.email]],
-      age: [this.user.age, [Validators.required, Validators.pattern(/^\d+/)]],
-      role: [this.user.role, [Validators.required]]
+      name: [
+        this.user.name,
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(15),
+        ],
+      ],
+      password: [
+        this.user.password,
+        [
+          Validators.required,
+          Validators.minLength(7),
+          Validators.maxLength(25),
+        ],
+      ],
+      email: [this.user.email, [Validators.required, emailValidator]],
+      age: [this.user.age, [Validators.required, rangeValidator(1, 122)]],
+      role: [this.user.role, [Validators.required]],
     });
-    this.userForm.valueChanges.subscribe(() => this.onValueChanged())
+    this.userForm.valueChanges.subscribe(() => this.onValueChanged());
   }
 
-  private onValueChanged(data?: any): void {
-    const form: any = this.userForm
+  onValueChanged(data?: any): void {
+    const form: any = this.userForm;
 
-    Object.keys(this.formErrors).forEach((field) => {
+    Object.keys(this.formErrors).forEach(field => {
       this.formErrors[field] = ' ';
 
       const control = form.get(field);
 
-      if (control && control.dirty && control.invalid) {
+      if ((control.dirty || control.touched) && control.invalid) {
         const message = this.validationMessages[field];
 
-        Object.keys(control.errors).forEach(key => this.formErrors[field] += message[key] + ' ')
-        }
-      
-    })
+        Object.keys(control.errors).some(key => this.formErrors[field] = message[key]);
+      }
+    });
   }
 }
